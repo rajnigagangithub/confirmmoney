f97a1a326813809745e30f6b645fb7bfb987f65e
rajnigagangithub <99281173+rajnigagangithub@users.noreply.github.com>
2025-06-29 18:09:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const otpRoutes = require('./routes/routes'); // fixed path here!
+//const otpRoutes = require('./routes/routes'); // fixed path here!
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
   res.send('ConfirmMoney Backend is running!');
 });
 // Use OTP routes
-app.use('/user', otpRoutes);
+//app.use('/user', otpRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`OTP API server running on port ${PORT}`));
