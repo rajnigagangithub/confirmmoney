5e628802b438c21b2a194df44ca5b5a4c29c447f
rajnigagangithub <99281173+rajnigagangithub@users.noreply.github.com>
2025-06-29 18:37:35 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,4 +1,11 @@
 const mysql = require('mysql2/promise');
+console.log('==============================');
+console.log('Database connection details:');
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASS:', process.env.DB_PASS ? '****' : '(empty)');
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('==============================');
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
