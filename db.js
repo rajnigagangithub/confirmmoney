ed4557e75612922498c653400241247db0926da7
rajnigagangithub <99281173+rajnigagangithub@users.noreply.github.com>
2025-06-29 18:05:44 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: 'localhost',       // change if needed
-  user: 'debian-sys-maint',            // your MySQL username
-  password: 'tnZtGkC5lFPs1vSe',    // your MySQL password
+  user: 'confirmmoney',            // your MySQL username
+  password: '58b2eb564d8965765b04',    // your MySQL password
   database: 'confirmmoney'       // your database name
 });
 
