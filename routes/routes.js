1aed60ac6309c2f201e50f507ba9d7b9440e3ed6
rajnigagangithub <99281173+rajnigagangithub@users.noreply.github.com>
2025-06-30 22:15:06 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { sendOtpHandler,updateUserInfoHandler,updateUserLoanHandler,
-    verifyOtpHandler,logoutHandler } = require('../api/api');
+    verifyOtpHandler,logoutHandler,getUserInfo } = require('../api/api');
 const authenticateToken = require('../middleware/auth');
 
 router.post('/send', sendOtpHandler);
@@ -9,6 +9,8 @@ router.post('/basic-details', authenticateToken, updateUserInfoHandler);
 router.post('/loan-preferences', authenticateToken, updateUserLoanHandler);
 router.post('/verify-otp', verifyOtpHandler);
 router.post('/logout', authenticateToken, logoutHandler);
+router.get('/get-user-info', authenticateToken, getUserInfo);
+
 
 
 
