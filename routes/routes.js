b92a4fa1b9691b8bc387820baeaaa2b4835c83c3
rajnigagangithub <99281173+rajnigagangithub@users.noreply.github.com>
2025-07-14 22:38:46 +0530

    Add files via upload

--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,41 @@
 const express = require('express');
+const cors = require('cors');
+
 const router = express.Router();
+const allowedOrigins = [
+  'https://confirm.money',
+  'https://www.confirm.money',
+  'http://localhost:3000'
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['POST'],
+  credentials: true
+};
+
 const { sendOtpHandler,updateUserInfoHandler,updateUserLoanHandler,
     verifyOtpHandler,logoutHandler,getUserInfo,userdownload,addLoanOfferHandler 
 ,getAllLoanOffersHandler,getLoanOfferByIdHandler,updateLoanOfferHandler} = require('../api/api');
 const authenticateToken = require('../middleware/auth');
 
-router.post('/send', sendOtpHandler);
-router.post('/basic-details', authenticateToken, updateUserInfoHandler);
-router.post('/loan-preferences', authenticateToken, updateUserLoanHandler);
-router.post('/verify-otp', verifyOtpHandler);
-router.post('/logout', authenticateToken, logoutHandler);
-router.get('/get-user-info', authenticateToken, getUserInfo);
-router.get('/downalduser', userdownload);
-router.post('/add-offers', addLoanOfferHandler);
-router.get('/offer-list', getAllLoanOffersHandler);
-router.get('/get-offer', getLoanOfferByIdHandler);
-router.post('/update-offers', updateLoanOfferHandler);
+router.post('/send', cors(corsOptions), sendOtpHandler);
+router.post('/basic-details', cors(corsOptions), authenticateToken, updateUserInfoHandler);
+router.post('/loan-preferences', cors(corsOptions), authenticateToken, updateUserLoanHandler);
+router.post('/verify-otp', cors(corsOptions), verifyOtpHandler);
+router.post('/logout', cors(corsOptions), authenticateToken, logoutHandler);
+router.get('/get-user-info', cors(corsOptions), authenticateToken, getUserInfo);
+router.get('/downalduser', cors(corsOptions), userdownload);
+router.post('/add-offers', cors(corsOptions),addLoanOfferHandler);
+router.get('/offer-list', cors(corsOptions), getAllLoanOffersHandler);
+router.get('/get-offer', cors(corsOptions),getLoanOfferByIdHandler);
+router.post('/update-offers', cors(corsOptions), updateLoanOfferHandler);
 
 
 
