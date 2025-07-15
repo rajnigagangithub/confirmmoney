49133a6b63cda63078658d24a6d7c7ea2db45f60
rajnigagangithub <99281173+rajnigagangithub@users.noreply.github.com>
2025-07-16 01:36:31 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -19,8 +19,10 @@ const corsOptions = {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['POST'],
-  credentials: true
+  
+ credentials: true,
+  methods: ['POST', 'OPTIONS']
+
 };
 
 const { sendOtpHandler,updateUserInfoHandler,updateUserLoanHandler,
@@ -39,6 +41,7 @@ router.post('/add-offers', cors(corsOptions),addLoanOfferHandler);
 router.get('/offer-list', cors(corsOptions), getAllLoanOffersHandler);
 router.get('/get-offer', cors(corsOptions),getLoanOfferByIdHandler);
 router.post('/update-offers', cors(corsOptions), updateLoanOfferHandler);
+router.options('/firebase-auth', cors(corsOptions));
 router.post('/firebase-auth', cors(corsOptions), otpverfification);
 
 
