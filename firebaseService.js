5b30edc0577bd8c94c8513d85b1e3168b9ebe478
rajnigagangithub <99281173+rajnigagangithub@users.noreply.github.com>
2025-07-16 07:15:51 +0530

    Update firebaseService.js

--- firebaseService.js
+++ firebaseService.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 
 // Replace with your Firebase Admin SDK service account key
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
