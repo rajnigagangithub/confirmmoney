6569395e0a58edab13cf8484e8df823aead8c51a
rajnigagangithub <99281173+rajnigagangithub@users.noreply.github.com>
2025-07-16 06:51:20 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const admin = require("../firebaseService");
+const admin = require("./firebaseService");
 
 const router = express.Router();
 const allowedOrigins = [
